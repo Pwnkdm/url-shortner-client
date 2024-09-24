1475f78ccc2d945fb19b44c19a7739953fac62ce
Pwnkdm <pawanrajkadam@gmail.com>
2024-09-24 22:41:33 +0530

    api changed

--- src/api/urlshortner.js
+++ src/api/urlshortner.js
@@ -1,27 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 
-const apiURL = 'https://odd-cyan-raven-boot.cyclic.app';
+const apiURL = "https://url-shortner-04u5.onrender.com";
 
-export const geturlDataRequest =(id)=>{
-    return axios.get(`${apiURL}/url/analytics`, {
-        headers: {
-          accept: "application/json",
-        },
-      });
-}
+export const geturlDataRequest = (id) => {
+  return axios.get(`${apiURL}/url/analytics`, {
+    headers: {
+      accept: "application/json",
+    },
+  });
+};
 
-export const posturlDataRequest =(payload)=>{
-    return axios.post(`${apiURL}/url`, payload, {
-        headers: {
-          accept: "application/json",
-        },
-      });
-}
+export const posturlDataRequest = (payload) => {
+  return axios.post(`${apiURL}/url`, payload, {
+    headers: {
+      accept: "application/json",
+    },
+  });
+};
 
-export const getAnalyticsRequest =()=>{
-    return axios.get(`${apiURL}/:shortId`, {
-        headers: {
-          accept: "application/json",
-        },
-      });
-}
+export const getAnalyticsRequest = () => {
+  return axios.get(`${apiURL}/:shortId`, {
+    headers: {
+      accept: "application/json",
+    },
+  });
+};
